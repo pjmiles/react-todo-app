e222d117d87d60421de567773dde763e7dbd9f7f
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-07-23 15:55:43 +0100

    form styled

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@
 import './App.css';
 import './components/Header'
 import Header from './components/Header';
+import TaskForm from './components/TaskForm';
+
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <TaskForm />
     </div>
   );
 }
