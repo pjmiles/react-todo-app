3f45089708764caa3cf6d087b043ebbc4cf9353a
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-16 11:11:34 +0100

    react-loading & iscompleted function

--- src/App.js
+++ src/App.js
@@ -5,27 +5,34 @@ import Header from "./components/Header";
 import TaskForm from "./components/TaskForm";
 import GetTasks from "./components/GetTasks";
 import axiosInstance from "./api/axios";
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [todos, setTodos] = useState([]);
 
+  const getTask = async () => {
+    try {
+      const data = await axiosInstance.get();
+      setTodos(data.data);
+      setLoading(false);
+    } catch {
+      console.log("Error fetching data");
+    }
+  };
   useEffect(() => {
-    const getTask = async () => {
-      try {
-        const data = await axiosInstance.get();
-        setTodos(data.data);
-      } catch {
-        console.log("Error fetching data");
-      }
-    };
-    getTask()
-  }, [])  
+    getTask();
+  }, []);
 
   return (
     <div className="App">
       <Header />
-      <TaskForm />
-      <GetTasks todos={todos} />
+      <TaskForm todos={todos} getTask={getTask}/>
+      {loading ? (
+        <ClipLoader size={100} />
+      ) : (
+        <GetTasks todos={todos} getTask={getTask} />
+      )}
     </div>
   );
 }
--- src/components/GetTasks.js
+++ src/components/GetTasks.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import axiosInstance from "../api/axios";
 
 const GetTasks = ({ getTask, todos }) => {
+  const [selected, setSelected] = useState(null);
+  const [isCompleted, setIsCompleted] = useState(false);
   const handleDelete = async (id) => {
     try {
       await axiosInstance.delete(id + "/");
@@ -9,7 +12,19 @@ const GetTasks = ({ getTask, todos }) => {
       console.log(error);
     }
   };
-  
+
+  const completedTodo = async (id, title, decriptions) => {
+    setSelected(id);
+    if (selected === id) {
+      setIsCompleted(!isCompleted);
+      await axiosInstance.put(id + "/", {
+        title: title,
+        decriptions: decriptions,
+        completed: isCompleted,
+      });
+    }
+  };
+
   // useEffect(() => {
   //   if(!mount){
   //     setMount(true)
@@ -26,7 +41,10 @@ const GetTasks = ({ getTask, todos }) => {
               <div className="task-title-container">
                 <h1 className="task-title">
                   {todo.title}
-                  <input type="checkbox" className="checkbox" />
+                  <input type="checkbox" className="checkbox"
+                  checked={selected === todo.id}
+                  onChange={() => completedTodo(todo.id, todo.title, todo.decriptions)} 
+                  />
                   <button
                     className="delete"
                     onClick={() => handleDelete(todo.id)}
