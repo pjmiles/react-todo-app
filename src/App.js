53230afe004afbfcdc3b62b0cea86594c5c0c8da
Ponmile Adebisi <ponmileadebisi@gmail.com>
2022-09-16 16:51:19 +0100

    footer added with external link

--- src/App.js
+++ src/App.js
@@ -6,10 +6,12 @@ import TaskForm from "./components/TaskForm";
 import GetTasks from "./components/GetTasks";
 import axiosInstance from "./api/axios";
 import ClipLoader from "react-spinners/ClipLoader";
+import { ExternalLink } from "react-external-link";
 
 function App() {
   const [loading, setLoading] = useState(true);
   const [todos, setTodos] = useState([]);
+  const [errMsg, setErrMsg] = useState("");
 
   const getTask = async () => {
     try {
@@ -17,23 +19,36 @@ function App() {
       setTodos(data.data);
       setLoading(false);
     } catch {
-      console.log("Error fetching data");
+      setErrMsg("Error fetching data, please try again!");
     }
   };
   useEffect(() => {
     getTask();
-  }, []);
+  });
 
   return (
-    <div className="App">
-      <Header />
-      <TaskForm todos={todos} getTask={getTask}/>
-      {loading ? (
-        <ClipLoader size={100} />
-      ) : (
-        <GetTasks todos={todos} getTask={getTask} />
-      )}
-    </div>
+    <>
+      <main className="App">
+        <Header />
+        <TaskForm todos={todos} getTask={getTask} />
+        {errMsg && <p className="error-message">{errMsg}</p>}
+        {loading ? (
+          <div className="spiner">
+            <ClipLoader size={80} className="spin-item" />
+          </div>
+        ) : (
+          <GetTasks todos={todos} getTask={getTask} />
+        )}
+      </main>
+      <footer>
+        <p>
+          Created with &#128157; by{" "}
+          <ExternalLink href="https://github.com/pjmiles" target="_blank" className="foot-link">
+            PJMILES
+          </ExternalLink>
+        </p>
+      </footer>
+    </>
   );
 }
 
